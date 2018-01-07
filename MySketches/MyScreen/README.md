96f82164fbcfa9af9aca865cf482758aa0505696
Bong <mscmacher@gmail.com>
2018-01-08 00:20:34 +0100

    Creata cartella per gli sketches personali

---
+++ MySketches/MyScreen/README.md
@@ -0,0 +1,19 @@
+# MyScreen
+
+**Compilazione, flashing su Arduino ed esecuzione**
+
+`$ make blink_test.hex`
+
+`$ gcc -o myScreen myScreen.c`
+
+**Monitorare i messaggi di output di Arduino con il mio programma**: `$ ./myScreen`
+
+In alternativa installare il tool più usato:
+
+`$ sudo apt-get install screen`
+
+`$ screen /dev/ttyACM0 115200`
+
+
+
+
---
+++ MySketches/MyScreen/myScreen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>    
+#include <string.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 300
+
+int main() {
+	char buf[BUF_SIZE];
+	int fd = open("/dev/ttyACM0", O_RDWR);
+	if (fd < 0) {
+		perror("error");
+		exit(-1);
+	}
+	
+	int bytes_read = 0;
+	while (1) {
+		int n = read(fd, buf+bytes_read, 1);
+		if (n <= 0) {
+			perror("error");
+			exit(-1);
+		}
+		if (buf[bytes_read] == '\0') {
+			printf("\n");
+		}
+		printf("%c", buf[bytes_read += (bytes_read%BUF_SIZE)]); // buffer circolare
+	}
+	
+	return 0;
+}
