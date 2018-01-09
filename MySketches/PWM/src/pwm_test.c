d9ed86f3d174b9ea973b6b940ea1c8939d75bae8
Bong <mscmacher@gmail.com>
2018-01-09 17:42:06 +0100

    Esempio di segnale analogico con la breadboard

---
+++ MySketches/PWM/src/pwm_test.c
@@ -0,0 +1,33 @@
+#include "digio.h"
+#include "pwm.h"
+#include "delay.h"
+#include <stdio.h>
+
+int pinLed = 6;
+
+int main(void){
+  // all pins as inputs with pull up resistors
+  PWM_init();
+  
+  // we enable all pwm outputs
+  PWM_enable(pinLed, 1);
+
+  uint8_t start_pin=0;
+  while(1) {
+    
+    for (int i=0; i<256; i++){
+      PWM_setDutyCycle(pinLed, i);
+	  delayMs(10);
+    } 
+    
+    delayMs(500);
+    
+	for (int i=255; i>=0; i--) {
+	  PWM_setDutyCycle(pinLed, i);
+	  delayMs(10);
+	}
+	
+	delayMs(500);
+	
+  }
+}
