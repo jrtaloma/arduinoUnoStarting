da53e7d36f7b74221f06ea4ad61d12d826fc1861
Bong <mscmacher@gmail.com>
2018-01-13 11:54:30 +0100

    Lettura degli input analogici tramite ADC interrupt

---
+++ MySketches/6.Analog_Input/README.md
@@ -0,0 +1,17 @@
+# Analog_Input
+
+Nel mondo reale grandezze come la temperatura e la pressione sono analogiche: la funzione del convertitore analogico digitale (ADC) è di convertire il segnale analogico registrato dal sensore in un segnale digitale perché possa essere elaborato dal microcontrollore, che è appunto digitale.
+
+In questo sketch l'MCU legge in input il valore registrato dalla fotoresistenza collegata al pin A5. L'ADC inzia la conversione e una volta completata genera un segnale che viene catturato. L'ISR legge il valore convertito (scala da 0 a 1023) e, se risulta minore di 128, accende per 1000 ms il LED collegato al pin 13 e invia un messaggio via seriale.
+
+**Cos'è una fotoresistenza e setup della breadboard**: [https://www.youtube.com/watch?v=ZEygo6f_70Q&index=10&list=PLu3ADb4nfjU27IitiNqskKRdfHhc-X2LQ](https://www.youtube.com/watch?v=ZEygo6f_70Q&index=10&list=PLu3ADb4nfjU27IitiNqskKRdfHhc-X2LQ)
+
+**Cos'è l'ADC**: [http://maxembedded.com/2011/06/the-adc-of-the-avr/](http://maxembedded.com/2011/06/the-adc-of-the-avr/)
+
+**AVR ADC interrupt**: [http://www.avrfreaks.net/forum/tut-c-newbies-guide-avr-adc?page=all](http://www.avrfreaks.net/forum/tut-c-newbies-guide-avr-adc?page=all)
+
+**Compilazione, flashing su Arduino ed esecuzione**
+
+`$ make adc_test.hex`
+
+**Monitorare i messaggi di output di Arduino con il mio programma**: `$ ./myScreen`
--- MySketches/6.Analog_Input/src/adc_test.c
+++ MySketches/6.Analog_Input/src/adc_test.c
@@ -4,6 +4,7 @@
 #include "adc.h"
 #include <stdio.h>
 #include <string.h>
+#include <avr/interrupt.h>
 
 #define HIGH 1
 #define LOW 0
@@ -13,6 +14,8 @@ int led = 13;
 
 struct UART* uart;
 
+volatile uint16_t adc_value;
+
 void printString(char* s){
 	int l=strlen(s);
 	for(int i=0; i<l; ++i, ++s)
@@ -20,27 +23,34 @@ void printString(char* s){
 	// UART_putChar(uart, (uint8_t) '\0'); // 'IMPORTANT: '\0' serves to mark the end of the string
 }
 
+// the ISR will be called when an A2D conversion completes
+ISR(ADC_vect) {
+	if (adc_value < 128) {
+		DigIO_setValue(led, HIGH);
+		char buffer[32];
+		sprintf(buffer, "adc_value: %d\n", adc_value);
+		printString(buffer);
+		delayMs(2000);
+	}
+	else {
+		DigIO_setValue(led, LOW);
+	}
+}
+
 int main(void){
-	uint16_t adc_value;
+	
+	cli(); // disable global interrupts
 	
 	DigIO_setDirection(led, Output); // initialize the digital pin as an output
 
-	adc_init(); // initialize adc
-
 	uart=UART_init("uart_0", 115200);
 	
+	adc_init(); // initialize adc and enable ADC interrupt
+	
+	sei(); // enable global interrupts
+	
 	while(1){
-		adc_value = adc_read(5); // read adc value at A5
-		if (adc_value < 128) {
-			DigIO_setValue(led, HIGH);
-			char buffer[32];
-			sprintf(buffer, "adc_value: %d\n", adc_value);
-			printString(buffer);
-			delayMs(2000);
-		}
-		else {
-			DigIO_setValue(led, LOW);
-		}	
+		adc_value = adc_read(5); // read adc value at A5	
 	}
 	
 	return 0;
