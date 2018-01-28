c77513db3d6f15ae708df2a0c6d9f56b42223c87
Bong <mscmacher@gmail.com>
2018-01-28 15:52:42 +0100

    Semaforo per non udenti: uso di timer e active buzzer

---
+++ MySketches/12.Semaphore/README.md
@@ -0,0 +1,9 @@
+# Semaforo per non udenti
+
+![GitHub Logo](semaphore.jpg)
+
+**Compilazione, flashing su Arduino ed esecuzione**
+
+`$ make semaphore.hex`
+
+Ogni 3 secondi un timer interrupt permette al semaforo di passare dal rosso, al verde e al giallo. Quando il semaforo è verde l'active buzzer emette un beep ogni 500 ms; quando è giallo un beep ogni 250 ms.
---
+++ MySketches/12.Semaphore/semaphore.c
@@ -0,0 +1,78 @@
+#include "digio.h"
+#include "delay.h"
+#include "timer.h"
+#include <stdio.h>
+#include <string.h>
+
+#define HIGH 1
+#define LOW 0
+
+int buzzerPin = 13;
+int redLED = 11;
+int yellowLED = 10;
+int greenLED = 9;
+
+volatile int redON = 0;
+volatile int yellowON = 0;
+volatile int greenON = 0;
+
+void timerFn(void* args){
+	if (yellowON || (!redON && !yellowON && !greenON)) {
+		yellowON = 0;
+		redON = 1;
+	}	
+		
+	else if (redON) {
+		redON = 0;
+		greenON = 1; 	
+	}
+	
+	else if (greenON) {
+		greenON = 0;
+		yellowON = 1;
+	}
+}
+
+int main(void){
+	
+	DigIO_setDirection(buzzerPin, Output);
+	DigIO_setDirection(redLED, Output);
+	DigIO_setDirection(yellowLED, Output);
+	DigIO_setDirection(greenLED, Output);
+	
+	struct Timer* timer=Timer_create("timer_0", 4000, timerFn, NULL);
+	Timer_start(timer);
+	
+	while(1) {
+		if (redON) {
+			DigIO_setValue(buzzerPin, LOW);
+			DigIO_setValue(yellowLED, LOW);
+			DigIO_setValue(redLED, HIGH);
+			while (redON) {}
+		}
+		
+		if (greenON) {
+			DigIO_setValue(redLED, LOW);
+			DigIO_setValue(greenLED, HIGH); 	
+			while (greenON) {
+				DigIO_setValue(buzzerPin, HIGH);
+				delayMs(500);
+				DigIO_setValue(buzzerPin, LOW);
+				delayMs(500);
+			}
+		}
+		
+		if (yellowON) {
+			DigIO_setValue(greenLED, LOW);
+			DigIO_setValue(yellowLED, HIGH);
+			while (yellowON) {
+				DigIO_setValue(buzzerPin, HIGH);
+				delayMs(250);
+				DigIO_setValue(buzzerPin, LOW);
+				delayMs(250);
+			}
+		}
+	}
+	
+	return 0;
+}
